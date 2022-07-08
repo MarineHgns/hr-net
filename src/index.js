335635bc8b843b0e445b19de77e347b662663e4a
Marine <marine.huygens@hotmail.fr>
2022-07-08 17:39:11 +0200

    return old router

--- src/index.js
+++ src/index.js
@@ -3,17 +3,14 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import { createRoot } from "react-dom/client";
 import reportWebVitals from "./reportWebVitals";
-import { unstable_HistoryRouter as HistoryRouter } from "react-router-dom";
-import { createBrowserHistory } from "history";
 const rootElement = document.getElementById("root");
 
 const root = createRoot(rootElement);
-const history = createBrowserHistory({ window });
 
 root.render(
-  <HistoryRouter history={history}>
+  <BrowserRouter>
     <App />
-  </HistoryRouter>
+  </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
